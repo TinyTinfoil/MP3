501f07d8f50433b34e97eaa58d6e9561fad67f84
Tinytinfoil <55459863+TinyTinfoil@users.noreply.github.com>
2023-10-02 14:46:21 -0400

    Basic line following example

--- MP3.ino
+++ MP3.ino
@@ -5,8 +5,17 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 // Select which 'port' M3 or M4
 Adafruit_DCMotor* motorL = AFMS.getMotor(3);
-Adafruit_DCMotor* motorL = AFMS.getMotor(4);
+Adafruit_DCMotor* motorR = AFMS.getMotor(4);
 
+uint8_t lsensor_pin = 0;
+uint8_t rsensor_pin = 1;
+uint16_t sensor_floor = 780;
+uint32_t time;
+uint32_t loop_time = 0;
+uint32_t LOOP_INTERVAL = 500;
+uint16_t displacement = 0;
+uint16_t angle_adjustment = 0;
+uint16_t zero_ptr = 0;
 void setup() {
   // put your setup code here, to run once:
   // start the serial port
@@ -18,19 +27,58 @@ void setup() {
     Serial.println("Could not find Motor Shield. Check wiring.");
     while (1);
   }
-
+  AFMS.begin();
   Serial.println("Motor Shield found.");
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  motorL->setSpeed(150);
-  motorL->run(FORWARD);
+  motorL->setSpeed(50);
+  motorL->run(RELEASE);
+  motorR->setSpeed(50);
+  motorR->run(RELEASE);
+  // motorL->run(RELEASE);
+  // motorR->run(RELEASE);
+  
   while (Serial.available() - 4 < 0) {}     //wait for data available (any)
   // turn on motor
-  motorL->run(RELEASE);
-  }
+  while (Serial.available() - 4 < 0) {}     //wait for data available (any)
+  loop_time = millis();
 
+  // PID(&displacement, &angle_adjustment, &zero_ptr, Kp, Ki, Kd, DIRECT) 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+uint8_t turn_scaling = 100;
+void loop() {
+  Serial.print(analogRead(0));
+  Serial.print(",");
+  Serial.println(analogRead(1));
+  time = millis();
+  // put your main code here, to run repeatedly:
+  displacement = calc_error();
+
+  motor_run_left(displacement);
+  motor_run_right(displacement);
+  while (time - loop_time > LOOP_INTERVAL) {
+    time = millis();
+  }
+  loop_time = millis();
+}
+
+uint16_t base_speed = 50;
+void motor_run_left(uint16_t error){
+  motorL->setSpeed(base_speed+error);
+  motorL->run(BACKWARD);
+}
+
+void motor_run_right(uint16_t error){
+  motorR->setSpeed(base_speed-error);
+  motorR->run(FORWARD);
+}
+
+uint16_t calc_error(){
+  // todo - remap sensor inputs to range
+  uint16_t lsensor = analogRead(lsensor_pin);
+  uint16_t rsensor = analogRead(rsensor_pin);
+  lsensor -= sensor_floor;
+  rsensor -= sensor_floor;
+  return lsensor - rsensor;
 }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Install the following from the Arduino Library manager & all dependancies:
 Adafruit motor shield V2
+PID
 
 M1, M2 0-7 Dead
 
